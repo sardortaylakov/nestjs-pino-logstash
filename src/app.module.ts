691825e3fb49020bd8dcf1dfe2e3b4a8b441d131
sardortaylakov <staylakov@tripactions.com>
2023-09-17 16:20:51 -0700

    use asynchronous configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,30 +8,36 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Module({
   imports: [
-    LoggerModule.forRoot({
-      pinoHttp: {
-        autoLogging: false,
-        base: undefined,
-        genReqId: () => uuidv4(),
-        messageKey: 'message',
-        formatters: {
-          level: (label: string) => ({ level: label.toUpperCase() })
-        },
-        customProps: (req: IncomingMessage) => {
-          return {
-            correlation_id: req.id,
-            request_method: req.method,
-            request_url: req.url,
-            environment: 'local'
-          };
-        },
-        serializers: {
-          req: () => undefined,
-          res: () => undefined,
-          err: (error) => error.stack
-        }
-      },
-      renameContext: 'logger_name'
+    ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          pinoHttp: {
+            autoLogging: false,
+            base: undefined,
+            genReqId: () => uuidv4(),
+            messageKey: 'message',
+            formatters: {
+              level: (label: string) => ({ level: label.toUpperCase() })
+            },
+            customProps: (req: IncomingMessage) => {
+              return {
+                correlation_id: req.id,
+                request_method: req.method,
+                request_url: req.url,
+                environment: config.get<string>('ENVIRONMENT', 'local')
+              };
+            },
+            serializers: {
+              req: () => undefined,
+              res: () => undefined,
+              err: (error) => error.stack
+            }
+          },
+          renameContext: 'logger_name'
+        };
+      }
     })
   ],
   controllers: [AppController],
